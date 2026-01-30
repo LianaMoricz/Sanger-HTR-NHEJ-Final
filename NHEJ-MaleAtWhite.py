ab95ba9b13dd30a407650bba4bf8ddbd137cb3dc
LianaMoricz <Lianamoricz@gmail.com>
2026-01-30 10:07:54 -0800

    nhej peak progress

--- NHEJ-MaleAtWhite.py
+++ NHEJ-MaleAtWhite.py
@@ -39,28 +39,32 @@ def printpeaks(peaks):
 
 def getextended(ab1seq, anchorend):
     newstart = anchorend + 42  #was 41
-    newend = anchorend + 45 #44
+    newend = anchorend + 79 #45 # was 44
     
     newab1sequence = ab1seq[newstart:newend + 1]
     
     return newstart, newend, newab1sequence
 
-def lookat4bp(ab1seq, startext, endext, seqext, peaks, similaritysequenceCR, similaritysequenceCS, simseqnhej1, simseqnhej2):
+def lookat4bp(ab1seq, startext, endext, seqext, peaks, similaritysequenceCS):
+    seqL = len(similaritysequenceCS)
     site = ab1seq[startext:startext + 4]
     print(f" 8th, 9th, 10th, 11th base =  {site}")
 
     print("\nBase-by-base COmparison @ each index:")
-    print("Index (8-11th) = CR Base Expected | CS Intact Base Expected | Other (NHEJ)  | % CR | % CS Intact | % Other (NHEJ) | ")
-    print("-" * 60)
+    print("Index (8-11th) = CS Intact Base Expected | Other Bases (NHEJ)  | % CS Intact | % Other | % Calc. NHEJ")
+    print("-" * 70)
 
     results = []
-    for i in range(4):
-        crbase = similaritysequenceCR[i] if i < len(similaritysequenceCR) else "N/A"
+    BaseList =  ['A', 'C', 'G', 'T']
+
+    for i in range(seqL):
         csbase = similaritysequenceCS[i] if i < len(similaritysequenceCS) else "N/A"
-        nhej1b = simseqnhej1[i] if i < len(simseqnhej1) else "N/A"
-        nhej2b = simseqnhej2[i] if i < len(simseqnhej2) else "N/A"
+       
+        PossibleNHEJBases = []
 
-
+        for eachbase in BaseList:
+            if eachbase != csbase:
+                PossibleNHEJBases.append(eachbase)
 
 
         if startext + i < len(peaks):
@@ -68,31 +72,49 @@ def lookat4bp(ab1seq, startext, endext, seqext, peaks, similaritysequenceCR, sim
             peakdict = {'A': A, 'C': C, 'G': G, 'T': T}
             totalpeaks = sum(peakdict.values())
 
-            percentageCR = (peakdict.get(crbase, 0) / totalpeaks * 100) if totalpeaks > 0 else 0.0
-            percentageCS = (peakdict.get(csbase, 0) / totalpeaks * 100) if totalpeaks > 0 else 0.0
-            percNHEJ1 = (peakdict.get(nhej1b, 0) / totalpeaks * 100) if totalpeaks > 0 else 0.0
-            percNHEJ2 = (peakdict.get(nhej2b, 0) / totalpeaks * 100) if totalpeaks > 0 else 0.0
-            totalnhej = ((peakdict.get(nhej1b, 0) + (peakdict.get(nhej2b, 0))) / totalpeaks) * 100 if totalpeaks > 0 else 0.0
 
-            NHEJpercentageFINAL = totalnhej * 2
+            #print(peakdict.get(csbase, 0))
+            PercentageCS = (peakdict.get(csbase, 0) / totalpeaks * 100) if totalpeaks > 0 else 0.0
+        
+            OtherCount = 0
+            for eachbase2 in PossibleNHEJBases:
+                OtherCount += (peakdict.get(eachbase2, 0))
+            
+            PercentageOther = OtherCount / totalpeaks * 100 if totalpeaks > 0 else 0.0
+
+            NHEJpercentageFINAL = PercentageOther * (4/3) 
+  
+
+
+
+            
+
+
 
         else:
-            percentageCR = 0.0
-            percentageCS = 0.0
-            difference = 0.0
+            #percentageCR = 0.0
+            PercentageCS = 0.0
+            #difference = 0.0
+            PercentageOther = 0.0
+            NHEJpercentageFINAL = 0.0
 
-        print(f"{i+1:5} | {crbase:7} | {csbase:7} | {nhej1b}/{nhej2b:7} | {percentageCR:6.1f} | {percentageCS:6.1f} | {NHEJpercentageFINAL:8.1f}")
-        # ['Index (8-11th)',  'CR Base Expected', 'CS Intact Base Expected', 'Other (NHEJ)', '% CR', '% CS Intact', '% Other (NHEJ)']
+
+        print(f"{i+1:5} | {csbase:7} | {PossibleNHEJBases} | {PercentageCS:6.1f} | {NHEJpercentageFINAL:8.1f}")
+        # ['Index (8-11th)', 'CS Intact Base Expected', 'Other Bases',  '% CS Intact', '% Other' , '% Calc. NHEJ']
 
         results.append({
-            'CRbase': crbase,
+            #'CRbase': crbase,
             'CSbase': csbase,
-            'NHEJ1base' : nhej1b,
-            'NHEJ2base' : nhej2b,
-            'percentageCR': percentageCR,
-            'percentageCS': percentageCS,
+            #'NHEJ1base' : nhej1b,
+            #'NHEJ2base' : nhej2b,
+            'OtherBases': PossibleNHEJBases,
+            #'percentageCR': percentageCR,
+            'PercentageCS': PercentageCS,
+            'PercentageOther': PercentageOther,
             'PercentageNHEJ': NHEJpercentageFINAL
         })
+    
+
 
     return results
 
@@ -122,10 +144,13 @@ def main():
 
     allresults = []
     samplenames = []
-    similaritysequenceCR = "TATC"
-    similaritysequenceCS = "CCGG"
-    simseqnhej1 = "AGCT"
-    simseqnhej2 = "GTAA"
+    #similaritysequenceCR = "TATC"
+    similaritysequenceCS = "TATCGCCATCCGGGAxTGCGACTGCTCAATGGCCAACCTGTGGACGCCAAGGAGATGCAGGCCAGGTGCGCCTATGTCCAG"
+    #"CCGG"
+    #simseqnhej1 = "AGCT"
+    #simseqnhej2 = "GTAA"
+
+    #If not CS, is probably an NHEJ, even if CS, 25% chance it is NHEJ if base is the same. ^^
     
     for filename in filenames:
         print(f"\nProcessing {os.path.basename(filename)}")
@@ -144,22 +169,24 @@ def main():
             anchorend = anchorstart + len(findpattern) - 1
     
             startext, endext, seqext = getextended(ab1seq, anchorend)
-            percentages = lookat4bp(ab1seq, startext, endext, seqext, peaks, similaritysequenceCR, similaritysequenceCS, simseqnhej1, simseqnhej2)            
+            percentages = lookat4bp(ab1seq, startext, endext, seqext, peaks, similaritysequenceCS)            
             allresults.append(percentages)
             
         except Exception as e:
             print(f"Error with {filename}: {str(e)}")
             #/N/A 4 bad sample read
             emptyresult = []
-            for i in range(4):
+            for i in range(len(similaritysequenceCS)):
                 emptyresult.append({
-                    'CRbase': "N/A",
+                    #'CRbase': "N/A",
                     'CSbase': "N/A",
-                    'NHEJ1base': "N/A",
-                    'NHEJ2base': "N/A",
-                    'percentageCR': 0.0,
-                    'percentageCS': 0.0,
-                    'PercentageNHEJ': 0.0
+                    'OtherBases' : "N/A",
+                    #'NHEJ1base': "N/A",
+                    #'NHEJ2base': "N/A",
+                    #'percentageCR': 0.0,
+                    'PercentageCS': "N/A",
+                    'PercentageOther':"N/A",
+                    'PercentageNHEJ': "N/A"
                 })
             allresults.append(emptyresult)
             continue
@@ -171,11 +198,14 @@ def main():
 
         for samplename, sampleresult in zip(samplenames, allresults):
             writer.writerow([samplename])
-            header = ['Index (8-11th)', 'CR Base Expected', 'CS Intact Base Expected', 'Other (NHEJ)', '% CR', '% CS Intact', '% Other (NHEJ)']
+            header = ['Index (8-11th)', 'CS Intact Base Expected', 'Other Bases', '% CS Intact', '% Other' , ' % Calc. NHEJ']
+            #print("Index (8-11th) = CS Intact Base Expected | Other Bases  | % CS Intact | % Other | % Calc. NHEJ")
+
             writer.writerow(header)
 
-            for i in range(4):
-                row = [i + 1, sampleresult[i]['CRbase'], sampleresult[i]['CSbase'], f"{sampleresult[i]['NHEJ1base']}/{sampleresult[i]['NHEJ2base']}", sampleresult[i]['percentageCR'], sampleresult[i]['percentageCS'], sampleresult[i]['PercentageNHEJ']]
+            for i in range(len(similaritysequenceCS)):
+                otherBstr = ','.join(sampleresult[i]['OtherBases']) if isinstance(sampleresult[i]['OtherBases'], list) else sampleresult[i]['OtherBases']
+                row = [i + 1, sampleresult[i]['CSbase'], otherBstr , sampleresult[i]['PercentageCS'], sampleresult[i]['PercentageOther'], sampleresult[i]['PercentageNHEJ']]
                 writer.writerow(row)
 
             writer.writerow([])
